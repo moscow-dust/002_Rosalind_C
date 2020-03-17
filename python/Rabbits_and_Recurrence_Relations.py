790ffcd4d5fa17971e77226ed66d3182a0720c50
moscow-dust <45701820+moscow-dust@users.noreply.github.com>
2020-03-17 23:26:35 +0300

    Update Rabbits_and_Recurrence_Relations.py
    I was bored and I modified this task: I counted execution time of funcion "sequence" and draw 3D plot describing relationship between amount of pairs and months and execution time

--- python/Rabbits_and_Recurrence_Relations.py
+++ python/Rabbits_and_Recurrence_Relations.py
@@ -1,3 +1,6 @@
+import time
+import plotly
+import plotly.graph_objs as go
 def sequence(months, pair):
     if months == 0:
         return 0
@@ -8,5 +11,29 @@ def sequence(months, pair):
 pair = 3
 months = 2
 print(sequence(months,pair))
+#count time of function execution  
+def count_time(f, arg1,arg2, n = 100):
+    t = float("inf")
+    for i in range(n):
+        t1_start = time.perf_counter()
+        f(arg1,arg2)
+        t1_stop = time.perf_counter()
+        t = min(t,t1_stop-t1_start)
+    return t
+#draw scatter plot 3D
+x = [i for i in range(1,21)]
+y = [j for j in range(1,21)]
+z = []
+print(x,y)
+for i in zip(x,y):
+    z.append(count_time(sequence,i[0], i[1]))
+fig = go.Scatter3d(x=x,y=y,z=z)
+mylayout = go.Layout(scene=dict(xaxis=dict(title="months"),
+                                yaxis= dict(title="pairs"),
+                                zaxis=dict(title="time")))
+plotly.offline.plot({"data": [fig],
+                     "layout": mylayout},
+                     auto_open=True,
+                     filename=("graf.html"))
 
 
