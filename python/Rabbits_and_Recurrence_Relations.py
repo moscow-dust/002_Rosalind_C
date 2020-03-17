1e4fd36f3ee4899e1e7dae5c145e7891bdf5e08c
moscow-dust <45701820+moscow-dust@users.noreply.github.com>
2020-03-17 23:48:28 +0300

    Update Rabbits_and_Recurrence_Relations.py

--- python/Rabbits_and_Recurrence_Relations.py
+++ python/Rabbits_and_Recurrence_Relations.py
@@ -7,7 +7,8 @@ def sequence(months, pair):
     elif months == 1:
         return 1
     else:
-        return(sequence((months-1), pair)+(sequence((months-2), pair)*pair))
+        return(sequence((months-1), 
+        pair)+(sequence((months-2), pair)*pair))
 pair = 3
 months = 2
 print(sequence(months,pair))
